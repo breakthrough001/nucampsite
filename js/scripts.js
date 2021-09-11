13bf4611eb962334b12b55982f8c8327f83016e3
Joe Naples <jnaples90@gmail.com>
2021-09-11 12:04:31 -0400

    Week 4 Assingment, added jquery modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function () {
+    // carousel
     $('.carousel').carousel({ interval: 2000 });
     $('#carouselButton').click(function () {
         if ($('#carouselButton').children('i').hasClass('fa-pause')) {
@@ -10,5 +11,15 @@ $(function () {
             $('#carouselButton').children('i').removeClass('fa-play');
             $('#carouselButton').children('i').addClass('fa-pause');
         }
+    });
+
+    // weekly workshop
+    $('#reserveButton').on('click', function () {
+        $('#reserveModal').modal('show');
+    });
+
+    $('#loginButton').on('click', function () {
+        console.log('click');
+        $('#loginModal').modal('show');
     });
 });
